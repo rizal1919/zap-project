6ca2739450e34aee12ecb9178f3d460ce4266acc
rizal1919 <rizalfathurr1999@gmail.com>
2023-09-06 21:09:16 +0700

    set

--- app/Livewire/Main/PositionComponent.php
+++ app/Livewire/Main/PositionComponent.php
@@ -16,11 +16,11 @@ class PositionComponent extends Component
         if(!Auth::check()){
             abort(403);
         }
-        $user     = Auth::user();
+        // $user     = Auth::user();
         $last_activity  = Activity::with('user_activity:id,username', 'place_activity:id,place')->where('user_id', $user->id)->latest()->first();
 
         $data = [
-            'user'          => $user,
+            // 'user'          => $user,
             'last_activity' => $last_activity
         ];
         return view('livewire.main.position-component', $data)->layout('layouts.template');
